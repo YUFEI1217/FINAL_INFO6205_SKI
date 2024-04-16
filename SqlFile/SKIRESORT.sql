9747b45dceb579ae437b284b4c5546c4759568fd
YUFEI1217 <chu.yuf@northeastern.edu>
2024-04-16 16:02:12 -0400

    Update SKIRESORT.sql
    Update all sql files

--- SqlFile/SKIRESORT.sql
+++ SqlFile/SKIRESORT.sql
@@ -11,11 +11,38 @@
  Target Server Version : 80031 (8.0.31)
  File Encoding         : 65001
 
- Date: 14/04/2024 16:14:26
+ Date: 16/04/2024 16:01:24
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for resort
+-- ----------------------------
+DROP TABLE IF EXISTS `resort`;
+CREATE TABLE `resort` (
+  `SkiResort` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Introduction` varchar(1450) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Location` varchar(145) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Website` varchar(145) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`SkiResort`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Records of resort
+-- ----------------------------
+BEGIN;
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Killington Ski Resort', 'Known as \"The Beast of the East,\" Killington offers a variety of terrain across six peaks. Famous for its long skiing season and vibrant après-ski scene.', '4763 Killington Road, Killington, VT 05751', 'https://www.killington.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Mountain Sunapee Resort', 'Family-friendly ski resort in New Hampshire with over 65 trails and slopes designed for various skill levels.', '1398 Route 103, Newbury, NH 03255', 'https://www.mountsunapee.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Nashoba Valley Ski Area', 'A popular small ski area in Massachusetts, known for its accessibility to Boston and for hosting a variety of winter activities.', '79 Powers Road, Westford, MA 01886', 'https://www.skinashoba.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Okemo Mountain Resort', 'Located in Vermont, Okemo is known for its family-friendly amenities, excellent snow quality, and a commitment to service and quality.', '77 Okemo Ridge Road, Ludlow, VT 05149', 'https://www.okemo.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Pats Peak Ski Area', 'A family-owned ski resort in New Hampshire, offering varied terrain, night skiing, and a range of seasonal activities.', '686 Flanders Road, Henniker, NH 03242', 'https://www.patspeak.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Stowe Mountain Resort', 'Premier ski destination in Vermont known for its challenging terrain, upscale amenities, and historic village.', '5781 Mountain Road, Stowe, VT 05672', 'https://www.stowe.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Stratton Mountain', 'Offers a top-rated ski and snowboard school in Vermont, with diverse terrain and vibrant village life.', '5 Village Lodge Road, Stratton Mountain, VT 05155', 'https://www.stratton.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Sunday River Resort', 'Known for its eight interconnected mountain peaks, extensive snowmaking, and year-round activities in Maine.', '15 South Ridge Road, Newry, ME 04261', 'https://www.sundayriver.com');
+INSERT INTO `resort` (`SkiResort`, `Introduction`, `Location`, `Website`) VALUES ('Wachusett Mountain Ski Area', 'Located in Massachusetts, Wachusett is known for its accessible location and night skiing, catering to families and day-trippers from Boston.', '499 Mountain Road, Princeton, MA 01541', 'https://www.wachusett.com');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for UserRate
@@ -51,6 +78,9 @@ INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALU
 INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('Chris', '2024-04-14 04:14:10', 'Wachusett Mountain Ski Area', 1.00, 'Bad');
 INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('An Lu', '2024-04-14 04:17:13', 'Wachusett Mountain Ski Area', 3.00, 'So badddd');
 INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('Yufei', '2024-04-14 15:44:45', 'Killington Ski Resort', 9.00, 'Nice but far');
+INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('12432', '2024-04-16 15:39:00', 'Pats Peak Ski Area', 3.00, 'Close too early');
+INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('qewq', '2024-04-16 15:43:32', 'Jay Peak Resort', 1.00, 'Nooo');
+INSERT INTO `UserRate` (`Username`, `Time`, `SkiResort`, `Rate`, `Content`) VALUES ('qewq', '2024-04-16 15:43:56', 'Wachusett Mountain Ski Area', 10.00, 'Goooood');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
